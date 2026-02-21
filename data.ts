264e583a9a361d1b58bf7d5a34d406f2d6b9d1e0
Eldrex Delos Reyes Bula <eldrexdelosreyesbula@gmail.com>
2026-02-22 07:33:28 +0800

    Update data.ts

--- data.ts
+++ data.ts
@@ -640,7 +640,7 @@ content:   `
 {
   id: 26,
   title: "Why Not You: A Personal Reflection on Courage and Action",
-  image: "post/Why%20Not%20You.gif",
+  image: BASE_IMAGE_URL + "post/Why%20Not%20You.gif",
   placeholderImage: getPlaceholder(26),
   date: "February 22, 2026",
   category: "Inspiration",
